60dc04df7512578f96c1dd7e90fb960bbac38c59
EugeniBykovski <bykovskieugeni@mail.ru>
2019-10-20 11:45:14 +0300

    this my multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var newFirst = first.split('').reverse();
+    var newSecond = second.split('').reverse();
+
+    var stack = [];
+
+    for (var i = 0; i < newFirst.length; i++) {
+        for (var j = 0; j < newSecond.length; j++) {
+            var m = newFirst[i] * newSecond[j];
+            stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+        }
+    }
+
+    for (var i = 0; i < stack.length; i++) {
+        var num = stack[i] % 10;
+        var move = Math.floor(stack[i] / 10);
+        stack[i] = num;
+
+        if (stack[i + 1])
+            stack[i + 1] += move;
+        else if (move != 0)
+            stack[i + 1] = move;
+    }
+
+    return stack.reverse().join('');
 }
